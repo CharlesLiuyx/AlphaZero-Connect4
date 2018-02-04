d823d3b8e52f558038e4a4df50972561a44f3bf3
YaoXing Liu <Charlesliuyx@outlook.com>
2018-02-04 12:45:55 -0800

    modify .gitignore

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config:
         self.resource = ResourceConfig()
 
         if config_type == "mini":
-            import connect4_zero.configs.mini as c
+            import configs.mini as c
         elif config_type == "normal":
-            import connect4_zero.configs.normal as c
+            import configs.normal as c
         else:
             raise RuntimeError(f"unknown config_type: {config_type}")
         self.model = c.ModelConfig()
